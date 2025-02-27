f22351c20f289ec998006aec1650a2906919f10e
harunandes21 <102485008+harunandes21@users.noreply.github.com>
2025-02-27 16:53:02 -0700

    Playlist modifying methods
    Added methods to create, and modify playlists. Minor other revisions are made. There are errors to resolve

--- src/database/MusicStore.java
+++ src/database/MusicStore.java
@@ -33,7 +33,7 @@ public class MusicStore {
 	
 	public Playlist getPlaylist(String name) {
 		for (Playlist p: playlists) {
-			if (p.getName() == name) {
+			if (p.getName().equalsIgnoreCase(name)) {
 				return p;
 			}
 		}
@@ -43,7 +43,7 @@ public class MusicStore {
 	public Song searchSongByName(String name) {
 		for (Album a : albums) {
 			for (Song s : a.getSongs()) {
-				if (s.getName() == name) {
+				if (s.getName().equalsIgnoreCase(name)) {
 					System.out.println("Song found: Album - " + s.getAlbum() + ", Title - "
 							+ s.getName() + ", Artist - " + s.getArtist());
 					return s;
@@ -57,7 +57,7 @@ public class MusicStore {
 	public Song searchSongByArtist(String name) {
 		for (Album a : albums) {
 			for (Song s : a.getSongs()) {
-				if (s.getArtist() == name) {
+				if (s.getArtist().equalsIgnoreCase(name)) {
 					System.out.println("Song found: Album - " + s.getAlbum() + ", Title - "
 							+ s.getName() + ", Artist - " + s.getArtist());
 					return s;
@@ -70,7 +70,7 @@ public class MusicStore {
 	
 	public Album searchAlbumByName(String name) {
 		for (Album a : albums) {
-			if (a.getTitle() == name) {
+			if (a.getTitle().equalsIgnoreCase(name)) {
 				String msg = "Artist: " + a.getArtist() + "\nAlbum: " + a.getTitle() + "\nSongs: \n";
 				for (Song song : a.getSongs()) {
 					msg += song.getName() + "\n";
@@ -85,7 +85,7 @@ public class MusicStore {
 	
 	public Album searchAlbumByArtist(String name) {
 		for (Album a : albums) {
-			if (a.getArtist() == name) {
+			 if (a.getArtist().equalsIgnoreCase(name)) {
 				String msg = "Artist: " + a.getArtist() + "\nAlbum: " + a.getTitle() + "\nSongs: \n";
 				for (Song song : a.getSongs()) {
 					msg += song.getName() + "\n";
@@ -98,7 +98,7 @@ public class MusicStore {
 		return null;
 
 	}
-	public static MusicStore Initializer(String path) throws IOException {
+	public static MusicStore initializer(String path) throws IOException {
 
 		MusicStore store = new MusicStore();
 		BufferedReader reader = new BufferedReader(new FileReader("albums/albums.txt"));
@@ -110,7 +110,7 @@ public class MusicStore {
 			String albumTitle = parts[0];
 			String artist = parts[1];
 			String albumFilePath = "albums/" + albumTitle + "_" + artist + ".txt";
-			Album album = AlbumReader(albumFilePath);
+			Album album = albumReader(albumFilePath);
 			store.albums.add(album);
 
 		}
@@ -118,9 +118,8 @@ public class MusicStore {
 		return store;
 	}
 	
-	private static Album AlbumReader(String filePath) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(filePath));
-
+	private static Album albumReader(String filePath) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
 		String data = reader.readLine();
 		String[] parts = data.split(",");
 		String title = parts[0];
@@ -134,7 +133,65 @@ public class MusicStore {
 			songs.add(new Song(songTitle, artist));
 		}
 		return new Album(title, artist, genre, year, songs);
-	}
+	    }
 
+	}
+	public void createPlaylist(String name) {
+	    
+	    for (Playlist p : playlists) {
+	        if (p.getName().equalsIgnoreCase(name)) {
+	            System.out.println("Playlist '" + name + "' already exists.");
+	            return;
+	        }
+	    }
+	    
+	    playlists.add(new Playlist(name, new ArrayList<>()));
+	}
+	public void addSongToPlaylist(String playlistName, String songName) {
+	    Playlist playlist = getPlaylist(playlistName);
+	    if (playlist == null) {
+	        System.out.println("Playlist '" + playlistName + "' not found.");
+	        return;
+	    }
+
+	    Song song = searchSongByName(songName);
+	    if (song == null) {
+	        System.out.println("Song '" + songName + "' not found in any album.");
+	        return;
+	    }
+
+	    playlist.addSong(song);
+	    System.out.println("Song '" + songName + "' added to playlist '" + playlistName + "'.");
+	}
+	public void removeSongFromPlaylist(String playlistName, String songName) {
+	    Playlist playlist = getPlaylist(playlistName);
+	    if (playlist == null) {
+	        System.out.println("Playlist '" + playlistName + "' not found.");
+	        return;
+	    }
+
+	    for (Song s : playlist.getSongs()) {
+	        if (s.getName().equalsIgnoreCase(songName)) {
+	            playlist.removeSong(s);
+	            System.out.println("Song '" + songName + "' removed from playlist '" + playlistName + "'.");
+	            return;
+	        }
+	    }
+	    System.out.println("Song '" + songName + "' not found in playlist '" + playlistName + "'.");
+	}
+	public void listAllPlaylists() {
+	    if (playlists.isEmpty()) {
+	        System.out.println("No playlists available.");
+	        return;
+	    }
+	    System.out.println("Playlists:");
+	    for (Playlist p : playlists) {
+	        System.out.println("Playlist: " + p.getName());
+	        for (Song s : p.getSongs()) {
+	            System.out.println(" - " + s.getName() + " by " + s.getArtist());
+	        }
+	    }
+	}
+	
 }
 
--- src/view/Main.java
+++ src/view/Main.java
@@ -4,6 +4,8 @@ import model.Album;
 import model.Song;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 import database.MusicStore;
 
@@ -14,15 +16,53 @@ public class Main {
 
 			String path = "albums/albums.txt";
 
-			MusicStore musicStore = MusicStore.Initializer(path);
+			MusicStore musicStore = MusicStore.initializer(path);
 
 			System.out.println("Music Store:");
 			for (Album album : musicStore.getAlbums()) {
 				System.out.println(album.getTitle() + "_" + album.getArtist());
 			}
-
+			Scanner scanner = new Scanner(System.in);
+	        String plName;
+	        
+	        
+			System.out.println("name for the new playlist:");
+	        plName = scanner.nextLine();
+	        musicStore.createPlaylist(plName);
+	        
+	        System.out.print("\nEnter the name of the album you want to search for: ");
+            String albumName = scanner.nextLine();
+            Album album = musicStore.searchAlbumByName(albumName);
+            if (album != null) {
+                // 3. List all songs in that album
+                System.out.println("\nSongs in the album '" + album.getTitle() + "':");
+                List<Song> songs = album.getSongs();
+                for (int i = 0; i < songs.size(); i++) {
+                    System.out.println((i + 1) + ". " + songs.get(i).getName());
+                }
+            }
+            System.out.println("\name of the song which will be added to playlist:");
+            String songName = scanner.nextLine();
+            musicStore.searchSongByName(songName);
+            musicStore.addSongToPlaylist(plName, songName);
+            
+            System.out.println("\name of the  second song which will be added to playlist:");
+            songName = scanner.nextLine();
+            musicStore.searchSongByName(songName);
+            musicStore.addSongToPlaylist(plName, songName);
+            
+            System.out.println("\nSongs in the playlist '" + plName + "':");
+            musicStore.listAllPlaylists();
+            
+            
+            
+            F
+            
+	        
 		} catch (IOException e) {
 			System.err.println("Error loading music store: " + e.getMessage());
 		}
+		
+		
 	}
 }
